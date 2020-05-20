1cd2dd0f63a8adaa437eda5c24fbd9c49ad57b3b
Pramod Thomas <pmod23@gmail.com>
2020-05-20 23:48:38 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,8 @@
+
 # Add your variables here
+first_number = 100
+second_number = 100
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number *  second_number
+quotient = first_number / second_number
